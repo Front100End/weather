830cfd79ea738536b956d5ee8d40046f3acc2774
seongjin cho <aktps12@gmail.com>
2022-06-28 19:27:39 +0900

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 function App() {
   return (
-    <div className="App">Weatherinfo 제대로 만들어보자 아자!!! 난 해낼거야</div>
+    <div className="App">
+      Weatherinfo 제대로 만들어보자 아자!!! 난 해낼거야 heroku 돼라
+      제발제발제발제발
+    </div>
   );
 }
 

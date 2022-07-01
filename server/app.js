a6ddec54b2abf41c64d3698eb24239be6c6c24fc
seongjin cho <aktps12@gmail.com>
2022-07-01 15:48:20 +0900

    weatherinfo API test

--- server/app.js
+++ server/app.js
@@ -47,23 +47,23 @@ app.post("/add-database", (req, res) => {
   res.send("post값이 정상적으로 추가 되었습니다.");
 });
 
-app.get("/weatherinfo", (req, res) => {
-  const res = async () => {
-    const x = 35.1333;
-    const y = 129.05;
-    try {
-      await axios
-        .get(
-          `${weatherBaseUrl}lat=${x}&lon=${y}&exclude=minutely&appid=${weather_API_KEY}`
-        )
-        .then((response) => {
-          console.log(response.data);
-        });
-    } catch (err) {
-      console.log("find error =>", err);
-    }
-  };
-});
+// app.get("/weatherinfo", (req, res) => {
+//   const res = async () => {
+//     const x = 35.1333;
+//     const y = 129.05;
+//     try {
+//       await axios
+//         .get(
+//           `${weatherBaseUrl}lat=${x}&lon=${y}&exclude=minutely&appid=${weather_API_KEY}`
+//         )
+//         .then((response) => {
+//           console.log(response.data);
+//         });
+//     } catch (err) {
+//       console.log("find error =>", err);
+//     }
+//   };
+// });
 
 app.listen(PORT, () => {
   console.log("server is running");

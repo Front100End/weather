7eef39602c09bafebe5eebf2b43741a6a79fbd25
seongjin cho <aktps12@gmail.com>
2022-07-06 11:27:17 +0900

    weatherinfo openAPI test

--- client/src/component/Main.js
+++ client/src/component/Main.js
@@ -103,7 +103,7 @@ const Main = (props) => {
         weatherinfo
       </button>
 
-      <Link to={{ pathname: `/search` }}>search 이동</Link>
+      <Link to={{ pathname: `/search` }}>search 이동하기</Link>
 
       <Nav
         toggleBtn={props.toggleBtn}
